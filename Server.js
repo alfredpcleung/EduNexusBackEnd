9054e363446c3d69c6536ac4bae6b47d5696dfe9
Sebastián Velasco <seguvega@hotmail.com>
2025-11-08 19:03:36 -0500

    Database

--- Server.js
+++ Server.js
@@ -1,9 +1,15 @@
 var Express = require("express");
+var cors = require('cors');
+var createError = require('http-errors');
+var logger = require('morgan');
+var configDb = require('./Config/db.js');
+
 
 var app = Express();
 
+configDb();
 
 
-app.Listen(3000, ()=> {
+app.listen(3000, ()=> {
     console.log("Server is running at  http://localhost:3000/");
 });
