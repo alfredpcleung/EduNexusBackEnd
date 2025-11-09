8b12201f008009fe70e2aa30f9e289869168ec9d
Sebastián Velasco <seguvega@hotmail.com>
2025-11-08 19:05:45 -0500

    Update Server Error handler

--- Server.js
+++ Server.js
@@ -6,8 +6,28 @@ var configDb = require('./Config/db.js');
 
 
 var app = Express();
-
 configDb();
+app.use(cors());
+app.use(logger('dev') );
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: false }));
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.json(
+    {
+      success: false,
+      message: err.message
+    }
+  );
+});
 
 
 app.listen(3000, ()=> {
